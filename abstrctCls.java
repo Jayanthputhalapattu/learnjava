4c08135eec2d559042a7e7c30fb68d7b2d5b7503
JAYANTH PUTHALAPATTU <45038110+Jayanthputhalapattu@users.noreply.github.com>
2020-01-21 17:07:31 +0530

    added code

--- abstrctCls.java
+++ abstrctCls.java
@@ -1,1 +1,29 @@
+abstract class kid{
+    String  name ;
+    int Licensno;
+     public void setName(name)
+     {
+         this.name= name;
+     }
+    public String getName()
+    {
+        return name;
+    }
+    public abstract void getLiceno();
+}
+class parent extends kid{
+    public void getLiceno(){
+       System.out.println("this is "); 
+    }
+}
+
+public class abstrctCls{
+  public static void main(String args[]){
+  
+    kid obj = new parent();
+    obj.getLiceno();
+    
+    
+  }
+}
 
