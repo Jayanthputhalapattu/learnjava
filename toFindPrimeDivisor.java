a856b5017c2ce496f8b14392fd709df740c7dd25
JAYANTH PUTHALAPATTU <45038110+Jayanthputhalapattu@users.noreply.github.com>
2020-01-29 23:12:41 +0530

    added hackerank basic mathematic question ,

---
+++ toFindPrimeDivisor.java
@@ -0,0 +1,50 @@
+
+import java.util.*;
+
+
+class sol{
+	int count = 0;
+	int pcount = 0;
+	public String isDiv(int r) {
+		int temp=0;
+		int ptemp = 0;
+		for (int i=1;i<=r;i++) {
+			if (r%i==0) {
+				temp++;
+			}
+		}
+		count = temp;
+		for  (int i=1;i<=count;i++) {
+			if (count%i==0) {
+				ptemp++;
+			}
+			
+		}
+		pcount = ptemp;
+		return pcount==2?"prime":"not prime";
+	}
+}
+
+
+public class bb {
+    public static void main(String args[] ) throws Exception {
+        Scanner sc=new Scanner(System.in);
+        
+        int t=sc.nextInt();
+       
+        sol obj = new sol();
+        int arr[]=new int[t];
+        for(int i=0;i<t;i++){
+            arr[i]=sc.nextInt();
+           
+        }
+        sc.close();
+        for(int i=0;i<t;i++){
+System.out.println(obj.isDiv(arr[i]));
+           
+        }
+        
+           
+    }
+    
+}
