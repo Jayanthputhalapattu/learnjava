e1cfba6f4875dec68a0b4bb95b24449c16f1000e
JAYANTH PUTHALAPATTU <45038110+Jayanthputhalapattu@users.noreply.github.com>
2020-02-29 23:21:53 +0530

    Run File for Queue

---
+++ RunnQueue.java
@@ -0,0 +1,20 @@
+package test;
+
+public class Runnq {
+  public static void main(String args[]) {
+	  Queue q  = new Queue();
+	  q.Dequeue();
+	  q.enque(20);
+	  q.enque(21);
+	  q.enque(4);
+	  q.enque(55);
+	  q.enque(22);
+ q.Dequeue();
+//	 q.Dequeue();
+	 q.enque(25);
+	 q.enque(44);
+	 q.enque(10);
+	  q.show();
+	  
+  }
+}
